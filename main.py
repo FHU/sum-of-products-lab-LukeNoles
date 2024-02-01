fb6788c972fac7507b5fc21e8536c20c4b9dc063
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2024-02-01 17:53:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def sum_of_products(list1, list2):
     for i in range(0, len(list1)):
-        newlist.append(int(list1[i]) * int(list2[i]))12
+        newlist.append(int(list1[i]) * int(list2[i]))
     return sum(newlist)
 
 if __name__ == '__main__':
