f4b3764c81db3f21990f9bcf067e7cfbe7ca246e
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2024-02-01 12:56:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-newlist=[]
-list1 = input()
-list2 = input()
 def sum_of_products(list1, list2):
     for i in range(0, len(list1)):
-        newlist.append(int(list1[i]) * int(list2[i]))
-    print (sum(newlist))
+        newlist.append(int(list1[i]) * int(list2[i]))12
+    return sum(newlist)
 
 if __name__ == '__main__':
+    newlist=[]
+    list1 = input()
+    list2 = input() 
     if len(list1) == len(list2):
-        sum_of_products(list1, list2)
+        
+        print (sum_of_products(list1, list2))
     else:
         print ("error")
