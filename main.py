4a7791d8deeb0aae09748a8ca304546cdb6dfa20
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2024-02-01 18:17:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def sum_of_products(list1, list2):
-    list1 =list1.split()
+    list1 = list1.split()
     list2 = list2.split()
     for i in range(0, len(list1)):
         newlist.append(int(list1[i]) * int(list2[i]))
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     newlist=[]
     list1 = input()
     list2 = input() 
-    if len(list1) == len(list2):
-        
+    if len(list1.split()) == len(list2.split()):
         print (sum_of_products(list1, list2))
     else:
         print ("error")
